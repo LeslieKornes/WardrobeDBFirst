4f723c98d21cf6c0ae1a95b80fbe3347047a8e6b
LeslieKornes <Lesliekornes@gmail.com>
2017-05-14 18:03:44 -0400

    first big commit

---
+++ Wardrobe/Controllers/ItemtypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wardrobe.Models;
+
+namespace Wardrobe.Controllers
+{
+    public class ItemtypesController : Controller
+    {
+        private WardrobeEntities db = new WardrobeEntities();
+
+        // GET: Itemtypes
+        public ActionResult Index()
+        {
+            return View(db.Itemtypes.ToList());
+        }
+
+        // GET: Itemtypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Itemtype itemtype = db.Itemtypes.Find(id);
+            if (itemtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemtype);
+        }
+
+        // GET: Itemtypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Itemtypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemTypeID,ItemType1")] Itemtype itemtype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Itemtypes.Add(itemtype);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemtype);
+        }
+
+        // GET: Itemtypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Itemtype itemtype = db.Itemtypes.Find(id);
+            if (itemtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemtype);
+        }
+
+        // POST: Itemtypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemTypeID,ItemType1")] Itemtype itemtype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemtype).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(itemtype);
+        }
+
+        // GET: Itemtypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Itemtype itemtype = db.Itemtypes.Find(id);
+            if (itemtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemtype);
+        }
+
+        // POST: Itemtypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Itemtype itemtype = db.Itemtypes.Find(id);
+            db.Itemtypes.Remove(itemtype);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Wardrobe/Controllers/WardrobeItemsController.cs
@@ -0,0 +1,145 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Wardrobe.Models;
+
+namespace Wardrobe.Controllers
+{
+    public class WardrobeItemsController : Controller
+    {
+        private WardrobeEntities db = new WardrobeEntities();
+
+        // GET: WardrobeItems
+        public ActionResult Index(int itemTypeId = 0)
+        {
+            var wardrobeItems = db.WardrobeItems.Include(w => w.Color).Include(w => w.Itemtype).Include(w => w.Occasion).Include(w => w.Season);
+
+            if (itemTypeId > 0)
+            {
+                wardrobeItems = wardrobeItems.Where(item => item.ItemTypeID == itemTypeId);
+            }
+            return View(wardrobeItems.ToList());
+        }
+
+        // GET: WardrobeItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            if (wardrobeItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wardrobeItem);
+        }
+
+        // GET: WardrobeItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1");
+            ViewBag.ItemTypeID = new SelectList(db.Itemtypes, "ItemTypeID", "ItemType1");
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1");
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1");
+            return View();
+        }
+
+        // POST: WardrobeItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WardrobeItemID,ItemTypeID,Name,Image,Type,ColorID,SeasonID,OccasionID")] WardrobeItem wardrobeItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WardrobeItems.Add(wardrobeItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1", wardrobeItem.ColorID);
+            ViewBag.ItemTypeID = new SelectList(db.Itemtypes, "ItemTypeID", "ItemType1", wardrobeItem.ItemTypeID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1", wardrobeItem.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1", wardrobeItem.SeasonID);
+            return View(wardrobeItem);
+        }
+
+        // GET: WardrobeItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            if (wardrobeItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1", wardrobeItem.ColorID);
+            ViewBag.ItemTypeID = new SelectList(db.Itemtypes, "ItemTypeID", "ItemType1", wardrobeItem.ItemTypeID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1", wardrobeItem.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1", wardrobeItem.SeasonID);
+            return View(wardrobeItem);
+        }
+
+        // POST: WardrobeItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WardrobeItemID,ItemTypeID,Name,Image,Type,ColorID,SeasonID,OccasionID")] WardrobeItem wardrobeItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(wardrobeItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1", wardrobeItem.ColorID);
+            ViewBag.ItemTypeID = new SelectList(db.Itemtypes, "ItemTypeID", "ItemType1", wardrobeItem.ItemTypeID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1", wardrobeItem.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1", wardrobeItem.SeasonID);
+            return View(wardrobeItem);
+        }
+
+        // GET: WardrobeItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            if (wardrobeItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wardrobeItem);
+        }
+
+        // POST: WardrobeItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            db.WardrobeItems.Remove(wardrobeItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
